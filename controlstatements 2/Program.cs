64e4b1c033853db6620d28059cc46102e7839a07
Admin <Admin@DESKTOP-6PQE64G>
2024-01-15 09:27:53 +0530

    Add project files.

---
+++ controlstatements 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter first numbe: ");
+        int firstnumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter second number:");
+        int secondnumber = int.Parse(Console.ReadLine());
+
+        bool isinunits = (firstnumber) % 10 == secondnumber;
+        bool isintens = (firstnumber / 10) % 10 == secondnumber;
+        bool isinhundreds = (firstnumber / 100) % 10 == secondnumber;
+        bool isinthousands = (firstnumber / 1000) % 10 == secondnumber;
+        Console.WriteLine($"{secondnumber} is {(isinunits?" ":" not")} present in units place of {firstnumber }");
+        Console.WriteLine($"{secondnumber} is {(isintens?" " :" not")} present in tens place of {firstnumber }");
+        Console.WriteLine($"{secondnumber} is {(isinhundreds?"" :"not")} present in hundreds place of {firstnumber }");
+        Console.WriteLine($"{secondnumber} is {(isinthousands?"" :"not")} present in thousands place of {firstnumber }");
+    }
+    }
